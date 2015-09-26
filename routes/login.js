acb3926f112a02fe182c4026c518d26103ce19a4
Steven <stpalmer21@gmail.com>
2015-09-25 23:34:43 -0400

    Possibly working login page

--- routes/login.js
+++ routes/login.js
@@ -1,24 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../database');
+var User = db.schemas.User;
 
 /* GET login page. */
 router.get('/', function(req, res, next) {
 
   res.render('login', {
     title: 'CMU Lifts',
-    loggedIn: true,
-    loggedIn: req.session.loggedIn
   });
-
-  if(req.session.loggedIn) {
-    res.render('search', {
-      title: 'CMU Lifts',
-      loggedIn: req.session.loggedIn
-    });
-  }
-
 });
 
+router.post('/', function(req, res, next) {
+  User.find({
+    id: req.params.id
+  }, function (err, users) {
+    if (err) {
+      console.error();(err);
+      return res.send(500);
+    }
+
+    if (users.length > 0) {
+      req.session.id = req.params.id;
+      req.session.loggedIn = true;
+      res.redirect('/search/');
+    } else {
+      res.render('login', {
+        title: 'CMU Lifts',
+        notFound: true,
+      });
+    }
+  })
+});
 
 
 
