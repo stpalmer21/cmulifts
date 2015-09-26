2631e211994693ef8c239665ed1c3bd6a05f6ed8
Steven <stpalmer21@gmail.com>
2015-09-25 21:54:10 -0400

    Editing login

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/login', login);
 
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/login.js
+++ routes/login.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('login', { title: 'CMU Lifts' });
 });
 
+router.post('/loginuser', function(req, res) {
+    //res.render('login', { title: "Welcome"});
+
+
+    console.log(req.body);
+    res.redirect('../')
+});
+
+
 module.exports = router;
