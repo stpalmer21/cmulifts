8fe154934ac7dfcc6421923c021e3b61428acd26
Zachary Snow <zachary.j.snow@gmail.com>
2015-09-26 11:41:22 -0400

    Functional skip button + code reuse in browse.js

--- public/javascripts/browse.js
+++ public/javascripts/browse.js
@@ -8,37 +8,40 @@ function displayUser(user) {
   $('span#workoutTime').text(user.workoutTime);
 }
 
-$(document).ready(function () {
-  if(users.length) {
+function showNextUser() {
+  if (users.length > 0) {
     displayUser(users[0]);
-    $('button#reject').click(function () {
-      users.shift();
-      $.post('/ajax/reject/' + $('span#name').data('id'), function () {
-        if(users.length) {
-          displayUser(users[0]);
-        }
-        else {
-          $('#user').fadeOut('fast');
-          $('#waiting').fadeIn('fast');
-        }
-      });
-    });
-    $('button#accept').click(function () {
-      users.shift();
-      $.post('/ajax/accept/' + $('span#name').data('id'), function (data) {
-        if(data['mutual'] === true) {
-          //TODO: redirect to contact us page
-        }
-        else {
-          if(users.length) {
-            displayUser(users[0]);
-          }
-          else {
-            $('#user').fadeOut('fast');
-            $('#waiting').fadeIn('fast');
-          }
-        }
-      });
-    });
   }
+  else {
+    $('#user').fadeOut('fast');
+    $('#waiting').fadeIn('fast');
+  }
+}
+
+$(document).ready(function () {
+  showNextUser();
+
+  $('button#reject').click(function () {
+    users.shift();
+    $.post('/ajax/reject/' + $('span#name').data('id'), function () {
+      showNextUser();
+    });
+  });
+
+  $('button#skip').click(function () {
+    users.shift();
+    showNextUser();
+  });
+
+  $('button#accept').click(function () {
+    users.shift();
+    $.post('/ajax/accept/' + $('span#name').data('id'), function (data) {
+      if (data['mutual'] === true) {
+        //TODO: redirect to contact us page
+      }
+      else {
+        showNextUser();
+      }
+    });
+  });
 });
