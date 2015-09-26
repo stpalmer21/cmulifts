8b043ecfff6eeec98fb6c978e1de79bb6603b465
Steven <stpalmer21@gmail.com>
2015-09-26 14:42:04 -0400

    my account page works

--- routes/my-account.js
+++ routes/my-account.js
@@ -3,13 +3,36 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('my-account', {
+
+  age = req.user.age;
+  gender = req.user.gender;
+  getRender(req, res, next);
+});
+
+function getRender(req, res, next) {
+  return res.render('my-account', {
     title: 'CMU Lifts',
-    userId: req.session.userId,
-    age: req.session.age,
-    gender: req.session.gender,
-    path: req.path
+    loggedIn: req.session.loggedIn,
+    path: req.path,
+    user: req.user,
   });
+}
+
+router.post('/', function(req, res, next) {
+  if (!req.session.loggedIn) {
+    return res.redirect('/login');
+  }
+
+  req.user.age = req.body.age;
+  req.user.gender = req.body.gender;
+
+  req.user.save(function (err) {
+    if (err) {
+      console.error(err);
+      return res.sendStatus(500);
+    }
+    getRender(req, res, next);
+  })
 });
 
 module.exports = router;
