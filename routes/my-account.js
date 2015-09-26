a5e3dc945c617f433e8a93296df468c718734e73
Zachary Snow <zachary.j.snow@gmail.com>
2015-09-26 13:08:41 -0400

    Fix branding + page titles throughout

--- routes/my-account.js
+++ routes/my-account.js
@@ -3,27 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
-
-  age = req.user.age;
-  gender = req.user.gender;
   getRender(req, res, next);
 });
 
 function getRender(req, res, next) {
   return res.render('my-account', {
-    title: 'CMU Lifts',
+    title: 'My Account',
     loggedIn: req.session.loggedIn,
     path: req.path,
-    user: req.user,
-=======
-  res.render('my-account', {
-    title: 'My Account',
-    userId: req.session.userId,
-    age: req.session.age,
-    gender: req.session.gender,
-    path: req.path
->>>>>>> 6b862f3d8c78bd92b1186b59bc4a8fa88180d761
+    user: req.user
   });
 }
 
